bf19710e0a5ea3f97abcc8b8c116fe8aafbf4690
felipefinity <felipe-augusto1997@hotmail.com>
2020-06-20 17:58:20 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -108,6 +108,14 @@ public class Order implements Serializable {
 		return result;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
